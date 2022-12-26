546e0f6619e8800c28649a1f85ace6a5a926b6f4
Prajjwal-Jaiswal <73015324+Prajjwal-Jaiswal@users.noreply.github.com>
2022-12-26 11:28:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -78,9 +78,9 @@ submit.addEventListener("click", (e) => {
 
 getWeather("Delhi")//BY DEFAULT CALLED FOR DELHI
 
-// function displayTime() {
-//     time = new Date();
-//     console.log(time);
-//     document.getElementById('time').innerHTML = time;
-// }
-// setInterval(displayTime, 1000);
+function displayTime() {
+    time = new Date();
+    console.log(time);
+    document.getElementById('time').innerHTML = time;
+}
+setInterval(displayTime, 1000);
